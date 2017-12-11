1af89f295858e344070918b14d9d169c897b5450
kuangjun <1441357891@qq.com>
2017-12-11 10:38:12 +0800

    提交5

---
+++ app/home/controller/Article.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\home\controller;
+class Article{
+    public function index(){
+        echo '跳转过来的';
+    }
+}
---
+++ system/model/Student.php
@@ -0,0 +1,8 @@
+<?php
+//定义命名空间
+namespace system\model;
+use kuangjiaming\model\Model;
+//让类Student继承Modele类
+class Student extends Model {
+
+}
